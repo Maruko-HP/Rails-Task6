a22a0814235ef0c83a963637eea28ed661e58adf
Maruko-HP <hp_maru@icloud.com>
2020-10-02 21:56:11 +0900

    Fakerを使用して初期データ2000件を投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+require "faker"
+Faker::Config.locale = :ja
+
+2000.times do
+  User.create!(name: Faker::Name.name, age: rand(0..100))
+end
+
+puts "初期データの投入に成功しました！"
